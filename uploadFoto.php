413f60ee48c5dfea86fd910bb72a2bb4a6b7aacb
retiarno <ari.retiarno@docotel.com>
2019-12-11 23:04:34 +0700

    change https ajax

--- uploadFoto.php
+++ uploadFoto.php
@@ -75,7 +75,7 @@ if ($terupload) {
 <html>
 <head>
     <title>Azure Computer Vision</title>
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
 </head>
 <body>
  
